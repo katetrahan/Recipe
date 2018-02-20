429469042e7f6628e0f155e826cecd65ee76c89b
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-02-19 16:40:16 -0800

    set up files/initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'app-root',
+  template: `
+  <h1>Recipe App</h1>`
+})
+
+export class AppComponent{
+
+}
---
+++ app/recipe.ts
@@ -0,0 +1,18 @@
+class Recipe {
+
+
+  constructor( public name: string, public ingredients: string, directions: string){
+
+  }
+
+}
+
+var recipes: Recipe[] = []
+recipes.push(new Recipe('Tacos', 'Tortilla, lettuce, tomatoes, beef, chiptole sauce, cheese, sour cream, olives', 'cook meat, put meat in Tortilla, put desired toppings on top of meat'));
+recipes.push(new Recipe('Mac n Cheese', 'noodles, milk, cheese, butter', 'boil water, put noodles in water, put noodles in strainer(removing water from noodles, add cheese and milk, stir and enjoy.)'));
+recipes.push(new Recipe('Cereal', 'Milk, Cereal', 'pour cereal in bowl, pour milk over cereal'));
+console.log(recipes);
+
+for(var recipe of recipes) {
+  console.log(recipe)
+}
