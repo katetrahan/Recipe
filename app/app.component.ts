cec22895a7ea58dd97016483a86d0004bd8e0b87
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-02-22 08:51:51 -0800

    add edit alert

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,22 +4,29 @@ import { Component } from '@angular/core';
 @Component ({
   selector: 'app-root',
   template: `
-  <h1>Recipe App</h1>
-  <h3>{{currentFood.name}}</h3>
-  <ul>
-    <li><strong>Name:</strong> {{currentFood.name}}</li>
-    <li><strong>Ingredients:</strong> {{currentFood.ingredients}}</li>
-    <li><strong>Directions:</strong> {{currentFood.directions}}</li>
-  </ul>`
+  <div class="container">
+    <h1>Recipe App</h1>
+    <h3>{{currentFood.name}}</h3>
+    <ul>
+      <li *ngFor ="let currentRecipe of recipes">{{currentRecipe.ingredients}} <button (click)="editRecipe()">Edit!</button></li>
+    </ul>
+  </div>`
 })
 
 export class AppComponent{
-  currentFood: Recipe = new Recipe('Tacos', "Tortilla, salsa, sour cream, cheese", "put ingredients in Tortilla");
+  currentFood: string = 'Recipes';
+  recipes: Recipe[] = [
+    new Recipe('Tacos', "Tortilla, salsa, sour cream, cheese", "put ingredients in Tortilla"),
+    new Recipe('Mac and Cheese', 'cheese, milk, butter, noodles', 'boil water, add noodles, add cheese and milk when noodles are soft, stir'),
+    new Recipe('Cereal', 'Cereal and Milk', 'put cereal in bowl and pour milk over cereal')
+  ];
 
+  editRecipe() {
+    alert("You requested to edit a recipe");
+  }
 }
 
 export class Recipe {
   constructor( public name: string, public ingredients: string, public directions: string){
-
   }
 }
